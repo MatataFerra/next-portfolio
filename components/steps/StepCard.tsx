15884fe7eeba86dec50ff6152ef1f6d21bec2a8b
matataferra <matias.agf@gmail.com>
2022-08-30 01:44:31 -0300

    better coding animation

--- components/steps/StepCard.tsx
+++ components/steps/StepCard.tsx
@@ -5,17 +5,23 @@ import styles from "./steps.module.scss";
 
 interface Props {
   children: React.ReactNode;
+  cssClass?: string;
+  title: string;
 }
 
-export const StepCardContainer: FC<Props> = ({ children }) => {
-  return <div className={styles.card}>{children}</div>;
+export const StepCardContainer: FC<Props> = ({ title, cssClass, children }) => {
+  return (
+    <div className={`${styles.card} ${cssClass && styles[cssClass]}`}>
+      <p className={styles.p}>{title}</p>
+      {children}
+    </div>
+  );
 };
 
 export const StepCardDrawIdeas: FC = () => {
   const pngImage = "/assets/images/steps/hand_pixel.png";
   return (
     <>
-      <p className={styles.p}>Step 1: Drawing your ideas</p>
       <div className={styles.line}>
         <span className={styles.span}>
           <HamIcon /> User
@@ -32,6 +38,46 @@ export const StepCardDrawIdeas: FC = () => {
       <div className={styles.image}>
         <Image src={pngImage} alt='hand' width={50} height={50} />
       </div>
+    </>
+  );
+};
+
+export const StepCardCoding: FC = () => {
+  return (
+    <>
+      <div className={styles["code-line-1"]} />
+      <div className={styles["code-container-2"]}>
+        <div className={styles["code-line-2"]} />
+        <div className={styles["code-line-3"]} />
+        <div className={styles["code-line-4"]} />
+      </div>
+      <div className={styles["code-line-5"]} />
+      <div className={styles["code-container-3"]}>
+        <div className={styles["code-line-6"]} />
+        <div className={styles["code-line-7"]} />
+      </div>
+      <div className={styles["code-container-4"]}>
+        <div className={styles["code-line-8"]} />
+      </div>
+      <div className={styles["code-container-5"]}>
+        <div className={styles["code-line-9"]} />
+        <div className={styles["code-line-10"]} />
+        <div className={styles["code-line-11"]} />
+      </div>
+      <div className={styles["code-line-12"]} />
+      <div className={styles["code-container-2"]}>
+        <div className={styles["code-line-13"]} />
+        <div className={styles["code-line-14"]} />
+        <div className={styles["code-line-15"]} />
+      </div>
+      <div className={styles["code-container-4"]}>
+        <div className={styles["code-line-16"]} />
+      </div>
+      <div className={styles["code-container-3"]}>
+        <div className={styles["code-line-17"]} />
+      </div>
+      <div className={styles["code-line-18"]} />
+      <div className={styles["code-line-19"]} />
     </>
   );
 };
--- components/steps/Steps.tsx
+++ components/steps/Steps.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import styles from "./steps.module.scss";
-import { StepCardContainer, StepCardDrawIdeas } from ".";
+import { StepCardCoding, StepCardContainer, StepCardDrawIdeas } from ".";
 
 export const Steps: FC = () => {
   return (
@@ -9,8 +9,11 @@ export const Steps: FC = () => {
         How do I create <span className={styles.gradient}>ideas</span>{" "}
       </h2>
       <div className={styles["card-container"]}>
-        <StepCardContainer>
+        <StepCardContainer title='Step 1: Drawing your ideas'>
           <StepCardDrawIdeas />
+        </StepCardContainer>
+        <StepCardContainer title='Step 2: Start coding!' cssClass='coding'>
+          <StepCardCoding />
         </StepCardContainer>
       </div>
     </>
